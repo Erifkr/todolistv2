658fcc358efef034371fbc07b84f1cb616bf6c30
Erifkr <99530574+Erifkr@users.noreply.github.com>
2023-03-17 22:13:54 +0530

    1st iteration

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+let items = [];
+let workItems = [];
+app.set('view engine', 'ejs');
+
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+
+
+
+app.get("/", function(req, res) {
+
+  let day = date.getDay();
+
+  res.render("lists", {
+    listTitle: day,
+    newListItems: items
+  });
+});
+app.post("/", function(req, res) {
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+
+  res.redirect("/");
+})
+app.get("/work", function(req, res) {
+  res.render("lists", {
+    listTitle: "work list",
+    newListItems: workItems
+  });
+});
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+app.listen(3000, function() {
+  console.log("server is running on port 3000!!");
+});
